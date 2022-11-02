b266ce52e547b82b4be67703ace68f3655ff7cea
Nix2107 <15808234+Nix2107@users.noreply.github.com>
2022-11-02 20:12:16 +0300

    I could get the code to work for one round. Next I will try to make it work for a game of 5 rounds

--- script.js
+++ script.js
@@ -1,29 +1,50 @@
 let choiceList = ["rock", "paper", "scissors"];
 let computerChoice = choiceList [Math.floor(Math.random()*choiceList.length)];
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     return computerChoice;
 }
 
-let computerSelection = (getComputerChoice());
+console.log (getComputerChoice());
 
-function playRound (computerSelection, playerSelection = prompt ("Enter Rock, Paper, or Scissors", ' ')) 
-{   
-    playerSelection = playerSelection.toLowerCase();
-    if (playerSelection == "scissors" && computerSelection == "rock") {
-        return "You lose! Rock beats Scissors";
-    } else if (playerSelection == "rock" && computerSelection == "paper") {
-        return "You lose! Paper beats Rock";
-    } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        return "You lose! Scissors beat Paper";
-    } else if (playerSelection == "paper" && computerSelection == "rock") { 
-        return "You win! Paper beats Rock";
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        return "You win! Scissors beat paper";
-    } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        return "You win! Rock beats scissors"
+function playRound (computerSelection = getComputerChoice(), playerSelection = prompt("Please Enter your choice: rock - paper - scissors?"))
+
+{   let playerChoice = playerSelection.toLowerCase();
+
+    if (playerChoice === "scissors" && computerSelection === "rock") {
+        computerScore += 1;
+        return `computer: ${computerScore}, you: ${playerScore}`;
+    } else if (playerChoice === "rock" && computerSelection === "paper") {
+        computerScore += 1;
+        return `computer: ${computerScore}, you: ${playerScore}`;
+    } else if (playerChoice === "paper" && computerSelection === "scissors") {
+        computerScore += 1;
+        return `computer: ${computerScore}, you: ${playerScore}`;
+    } else if (playerChoice === "paper" && computerSelection === "rock") { 
+        playerScore += 1;
+        return `computer: ${computerScore}, you: ${playerScore}`;
+    } else if (playerChoice === "scissors" && computerSelection === "paper") {
+        playerScore += 1;
+        return `computer: ${computerScore}, you: ${playerScore}`;
+    } else if (playerChoice === "rock" && computerSelection === "scissors") {
+        playerScore += 1;
+        return `computer: ${computerScore}, you: ${playerScore}`;
     } else {
-        return "It's a draw!"
+        computerScore = 0;
+        playerScore = 0;
+        return `It's a draw! computer: ${computerScore}, you: ${playerScore}`
     }
 
 }
+
+function game() 
+
+{
+    for (let numberOfRounds = 0; numberOfRounds < 5; numberOfRounds++) 
+    {
+        console.log (playRound());
+        
+    }
+} 
