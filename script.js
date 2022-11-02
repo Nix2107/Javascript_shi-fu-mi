ebcd43c59cfbfe18860679edba7e324f50559b7a
Nix2107 <15808234+Nix2107@users.noreply.github.com>
2022-11-02 06:04:16 +0300

    added the function for one round, but I couldn't get to work properly yet, there's probably a logical error

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
+let choiceList = ["rock", "paper", "scissors"];
+let computerChoice = choiceList [Math.floor(Math.random()*choiceList.length)];
+
 function getComputerChoice() {
-    let choiceList = ["rock", "paper", "scissors"];
-    let computerChoice = choiceList [Math.floor(Math.random()*choiceList.length)];
     return computerChoice;
+}
+
+let computerSelection = (getComputerChoice());
+
+function playRound (computerSelection, playerSelection = prompt ("Enter Rock, Paper, or Scissors", ' ')) 
+{   
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "You lose! Rock beats Scissors";
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        return "You lose! Paper beats Rock";
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "You lose! Scissors beat Paper";
+    } else if (playerSelection == "paper" && computerSelection == "rock") { 
+        return "You win! Paper beats Rock";
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "You win! Scissors beat paper";
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You win! Rock beats scissors"
+    } else {
+        return "It's a draw!"
+    }
+
 }
