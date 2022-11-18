6364c81d3be02a4d39ddeec1f14707793f81db09
Nix2107 <15808234+Nix2107@users.noreply.github.com>
2022-11-18 08:02:54 +0300

    I have a basic code that makes the shi-fu-mi program work for 5 rounds although there is still an issue about the function for getting the computer choice

--- script.js
+++ script.js
@@ -1,56 +1,102 @@
-let choiceList = ["rock", "paper", "scissors"];
-let computerChoice = choiceList [Math.floor(Math.random()*choiceList.length)];
+let ChoiceList = ["rock", "paper", "scissors"];
+let computerSelection = ChoiceList [Math.floor(Math.random()*ChoiceList.length)];
 let playerScore = 0;
 let computerScore = 0;
+let roundNumber = 0;
 
-function getComputerChoice() {
-    return computerChoice;
+function getComputerChoice ()
+{
+    return computerSelection;
 }
 
 console.log (getComputerChoice());
 
-function playRound (computerSelection = getComputerChoice(), playerSelection = prompt("Please Enter your choice: rock - paper - scissors?"))
+function playRound (computerSelection = getComputerChoice(), playerSelection)
 
-{   let playerChoice = playerSelection.toLowerCase();
+{  
+    roundNumber += 1
 
-    if (playerChoice === "scissors" && computerSelection === "rock") {
+    if (playerSelection === "scissors" && computerSelection === "rock") 
+    {
         computerScore += 1;
-        return `You lost the round! current score is computer: ${computerScore}, you: ${playerScore}`;
-    } else if (playerChoice === "rock" && computerSelection === "paper") {
+        return `You lost the round! current score is 
+        computer: ${computerScore}
+        you: ${playerScore}`;
+
+    } else if (playerSelection === "rock" && computerSelection === "paper") 
+    {
         computerScore += 1;
-        return `You lost the round! current score is computer: ${computerScore}, you: ${playerScore}`;
-    } else if (playerChoice === "paper" && computerSelection === "scissors") {
+        return `You lost the round! current score is 
+        computer: ${computerScore}
+        you: ${playerScore}`;
+
+    } else if (playerSelection === "paper" && computerSelection === "scissors") 
+    {
         computerScore += 1;
-        return `You lost the round! current score is computer: ${computerScore}, you: ${playerScore}`;
-    } else if (playerChoice === "paper" && computerSelection === "rock") { 
+        return `You lost the round! current score is 
+        computer: ${computerScore}
+        you: ${playerScore}`;
+
+    } else if (playerSelection === "paper" && computerSelection === "rock") 
+    { 
+        playerScore +=1;
+        return `You won the round! current score is 
+        computer: ${computerScore}
+        you: ${playerScore}`;
+
+    } else if (playerSelection === "scissors" && computerSelection === "paper") 
+    {
         playerScore += 1;
-        return `You won the round! current score is computer: ${computerScore}, you: ${playerScore}`;
-    } else if (playerChoice === "scissors" && computerSelection === "paper") {
-        playerScore += 1;
-        return `You won the round! current score is computer: ${computerScore}, you: ${playerScore}`;
-    } else if (playerChoice === "rock" && computerSelection === "scissors") {
-        playerScore += 1;
-        return `You won the round! current score is computer: ${computerScore}, you: ${playerScore}`;
-    } else {
-        return `It's a draw! current score is computer: ${computerScore}, you: ${playerScore}`
+        return `You won the round! current score is 
+        computer: ${computerScore}
+        you: ${playerScore}`;
+
+    } else if (playerSelection === "rock" && computerSelection === "scissors") 
+    {   
+        playerScore +=1;
+        return `You won the round! current score is 
+        computer: ${computerScore}
+        you: ${playerScore}`;
+
+    } else 
+    {
+        return `It's a draw! current score is 
+        computer: ${computerScore}
+        you: ${playerScore}`;
+        
     }
 
+};
+
+function endGame() 
+{
+    if (playerScore < computerScore) 
+    {
+        return `You lost the game :( your score is ${playerScore}, and computer's score is ${computerScore}`;   
+    } else if (playerScore > computerScore)
+    {
+        return `You win the game :) your score is ${playerScore}, and computer's score is ${computerScore}`;
+    } else if (playerScore === computerScore)
+    {
+        return `It's a draw! you have the same score as the computer`;
+    }
+    
 }
 
+const scorepara = document.querySelector ("#scorep");
+const resultpara = document.querySelector ("#resultp");
 
-function playGame() 
+const playerChoiceRock = document.querySelector ("#rock");
+playerChoiceRock.addEventListener ('click', () => {
+    scorepara.textContent = playRound(computerSelection, "rock")
+    if (roundNumber === 5) {resultpara.textContent = endGame () }});
 
-{
-    for (let numberOfRounds = 0; numberOfRounds < 5; numberOfRounds++) 
-    {   
-        console.log (playRound());    
-    }
-    if (playerScore < computerScore) {
-        return "You lost the game :( ";
-    } else if (playerScore == computerScore){
-        return "It's a draw, you have the same score";
-    } else if (playerScore > computerScore) {
-        return "Yayyy, you won the game!! :D ";
-    }
+const playerChoicePaper = document.querySelector ("#paper");
+playerChoicePaper.addEventListener ('click', () => {
+    scorepara.textContent = playRound(computerSelection, "paper")
+    if (roundNumber === 5) {resultpara.textContent = endGame () }});
 
-} 
+const playerChoiceScissors = document.querySelector ("#scissors");
+playerChoiceScissors.addEventListener ('click', () => {
+    scorepara.textContent = playRound(computerSelection, "scissors")
+    if (roundNumber === 5) {resultpara.textContent = endGame () }});
